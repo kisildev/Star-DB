c4571a79eb3c5fee413b2d98c1caacd939529649
kisildev <sanya.kisil96@gmail.com>
2019-07-05 12:44:51 +0300

    'transform_swapiServise'

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,38 +4,30 @@ import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
-    constructor() {
-        super();
-        this.updatePlanet();
-    }
-
     swapiService = new SwapiService();
 
     state = {
-        id: null,
-        name: null,
-        population: null,
-        rotationPeriod: null,
-        diameter: null
+        planet: {}
+    };
+
+    constructor() {
+        super();
+        this.updatePlanet();
+    };
+
+    onPlanetLoaded = (planet) => {
+        this.setState({planet});
     };
 
     updatePlanet() {
-        const id = 2;
+        const id = Math.floor(Math.random()*25) + 2;
         this.swapiService
             .getPlanet(id)
-            .then((planet)=> {
-                this.setState({
-                    id,
-                    name: planet.name,
-                    population: planet.population,
-                    rotationPeriod: planet.rotation_period,
-                    diameter: planet.diameter
-                })
-            });
-    }
+            .then(this.onPlanetLoaded);
+    };
 
     render() {
-        const { id, name, population, rotationPeriod, diameter } = this.state;
+        const { planet : { id, name, population, rotationPeriod, diameter } } = this.state;
 
         return (
           <div className='random-planet'>
